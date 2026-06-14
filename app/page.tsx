05be6195ec5197caf9c1a3d07fb484fee74cf0e6
GlodiMietete <glodimietete@gmail.com>
2026-06-14 20:06:01 +0200

    Move the navbar to leyout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import Navbar from "@/components/Navbar";
-
 export default function Home() {
   return (
     <>
-      <Navbar />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
         <h1 className="text-5xl font-bold">Bienvenue sur IAM Lite</h1>
